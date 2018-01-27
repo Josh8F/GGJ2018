d8d794cf8096a464865afb034d23bba1e15d80c3
Josh8F <joshfranx13@gmail.com>
2018-01-27 13:44:45 -0600

    Switches

---
+++ Assets/Scripts/ControllerMinigame.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerMinigame : MonoBehaviour {
+
+	public GameObject[] switches;
+	public GameObject[] luces;
+	public int activo = 0;
+	string clave;
+	// Use this for initialization
+	void Start () {
+		transform.position = new Vector3(switches[activo].transform.position.x,switches[activo].transform.position.y,transform.position.z);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.RightArrow)){
+			activo++;
+		}
+		if(Input.GetKeyDown(KeyCode.LeftArrow)){
+			activo--;
+		}
+		activo = Mathf.Clamp(activo,0,3);
+		if(Input.GetKeyDown(KeyCode.Space)){
+			luces[activo].GetComponent<Light>().enabled = !luces[activo].GetComponent<Light>().enabled;
+			clave += activo;
+		}
+		int encendidos = 0;
+		foreach(GameObject luz in luces){
+			if(luz.GetComponent<Light>().isActiveAndEnabled){
+				encendidos++;
+			}
+		}
+		if(encendidos == 4){
+			if(clave == "1423"){
+				
+			}
+		}
+		transform.Translate(switches[activo].transform.position - transform.position);
+	}
+}
