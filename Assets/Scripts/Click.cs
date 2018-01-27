e986c3d9401a7365e2f00d1e6da23c89ae2f7db5
Josh8F <joshfranx13@gmail.com>
2018-01-27 08:58:23 -0600

    Raycast

---
+++ Assets/Scripts/Click.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Click : MonoBehaviour {
+
+	Ray interaccion;
+	RaycastHit hit;
+	public LayerMask interactuable;
+	public float distancia = 150;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		interaccion = new Ray(transform.position, transform.forward);
+		if(Physics.Raycast(interaccion, out hit, interactuable)){
+			if(Input.GetMouseButtonDown(0)){
+				
+			}
+		}
+	}
+}
