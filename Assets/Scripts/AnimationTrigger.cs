5d68d7441b18ae50e3dd0f03f8b4bbf074c6a37f
Brim502 <brim.carrera@gmail.com>
2018-01-28 10:58:03 -0600

    animations2

---
+++ Assets/Scripts/AnimationTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationTrigger : MonoBehaviour
+{
+
+    public string animationToPlay;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+
+            other.gameObject.GetComponentInChildren<Animator>().SetBool(animationToPlay, true);
+
+
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.gameObject.GetComponentInChildren<Animator>().SetBool(animationToPlay, false);
+            //Destroy(GetComponent<AnimationTrigger>());
+        }
+
+    }
+}
