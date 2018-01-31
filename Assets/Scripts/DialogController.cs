14281be9bf4711f74a091873fd2534ff1815a2b9
Josh8F <joshfranx13@gmail.com>
2018-01-31 11:37:28 -0600

    DijoConsternadoRyan

--- Assets/Scripts/DialogController.cs
+++ Assets/Scripts/DialogController.cs
@@ -15,7 +15,7 @@ public class DialogController : MonoBehaviour
     public string[] lstDialogs = {
         "¡Demonios! ¡En empresas tan prominentes no se debería ir la luz de esta forma! Mi trabajo está... Algo debió haber pasado.",
         "Algo huele raro, debo seguir adelante.",
-        "Uniforme número “3561”…-dijo consternado, Ryan-¿Pero qué diablos ha pasado aquí?",
+        "Uniforme número “3561”… ¿Pero qué diablos ha pasado aquí?",
         "Tres veces indica el número tres, debería buscar algo relacionado a eso.",
         "Recuerdo haber visto esos números hace un momento. ¡Es cierto! ¡El uniforme chamuscado tenía los números “3561” inscritos en él! Recuerdo que Angus vestía ese uniforme. También recuerdo que su oficina era la 5.",
         "“Día 27/01/2018, hoy se ha sido asignado verificar el estado de la caja de electricidad. Se dice que ha estado inestable, pero de seguro es un problema menor.”",
